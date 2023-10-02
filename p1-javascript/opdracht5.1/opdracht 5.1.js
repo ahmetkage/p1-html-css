8f558d1d6a606d771b029b138b45cbbcadbe43f9
ahmetkage <9021567@student.zadkine.nl>
2023-10-02 15:13:58 +0200

    js

---
+++ p1-javascript/opdracht5.1/opdracht 5.1.js
@@ -0,0 +1,9 @@
+function changecollor() 
+{
+    document.getElementById("kleur").style.color = "red";
+}
+
+function changecollor2() 
+{
+    document.getElementById("kleur2").style.color = "red";
+}
