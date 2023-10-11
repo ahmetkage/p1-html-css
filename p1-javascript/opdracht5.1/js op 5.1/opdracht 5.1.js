aba591de24c1f00ded46c91bcd1992eacdfbb6aa
ahmetkage <9021567@student.zadkine.nl>
2023-10-11 10:59:03 +0200

    6.3

--- p1-javascript/opdracht5.1/js op 5.1/opdracht 5.1.js
+++
@@ -1,9 +0,0 @@
-function changecollor() 
-{
-    document.getElementById("kleur").style.color = "red";
-}
-
-function changecollor2() 
-{
-    document.getElementById("kleur2").style.color = "red";
-} 
